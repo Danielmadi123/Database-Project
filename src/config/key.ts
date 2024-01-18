d9a3fb7467b8a60cd5b64cc5bccec615cee20717
Danielmadi123 <139677305+Danielmadi123@users.noreply.github.com>
2024-01-18 15:07:37 +0200

    key.ts

--- src/config/key.ts
+++ src/config/key.ts
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURL:
-    "mongodb+srv://Maidan2023:M71pqaokM34QNxJ0@hackeru1.45yqk8d.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://mongodb+srv://daniel158.lfn95ll.mongodb.net/?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
